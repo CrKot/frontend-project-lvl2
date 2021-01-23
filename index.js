78a4a6c124a3d58a56397f8b110092dc28718a72
artemkrv <artemsalikhov@mail.ru>
2021-01-23 22:48:50 +0700

    trying to pass tests Hexlet

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 // @ts-check
 
-import commander from 'commander';
-import getDiff from './src/index.js';
-
-const gendiff = new commander.Command();
-gendiff
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<pathToFile1> <pathToFile2>')
-  .action((pathToFile1, pathToFile2) => {
-    const diffTo2Files = getDiff(pathToFile1, pathToFile2, gendiff.opts().format);
-    console.log(diffTo2Files);
-    return diffTo2Files;
-  });
+import gendiff from './src/index.js';
 
 export default gendiff;
