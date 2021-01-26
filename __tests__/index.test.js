a7aab172d21eca38289e55b1682bb0ee8bb247fa
artemkrv <artemsalikhov@mail.ru>
2021-01-26 17:19:30 +0700

    add test.each rm fixtures not used files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import path, { dirname } from 'path';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import gendiff from '../src/index.js';
@@ -7,55 +7,25 @@ import gendiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '/', '__fixtures__', filename);
-const getFile = (pathFile) => fs.readFileSync(pathFile, 'utf8');
-
+const readFile = (pathFile) => fs.readFileSync(pathFile, 'utf8');
+const expectPlain = readFile(getFixturePath('expectPlain.txt'));
+const expectStylish = readFile(getFixturePath('expectRecursionTree.txt'));
 const recursionTree1Yml = getFixturePath('recursionTreeFile1.yml');
 const recursionTree2Yml = getFixturePath('recursionTreeFile2.yml');
+const recursionTree1Json = getFixturePath('complexFile1.json');
+const recursionTree2Json = getFixturePath('complexFile2.json');
 
-test('gendiff JSON format Stylish', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const testExpect = getFile(getFixturePath('filesExpect.json'));
-  const result = gendiff(file1, file2);
-
-  expect(result).toEqual(testExpect);
-});
-
-test('gendiff YAML format Stylishs', () => {
-  const file1 = getFixturePath('file1.yml');
-  const file2 = getFixturePath('file2.yml');
-  const testExpect = getFile(getFixturePath('filesExpect.yml'));
-  const result = gendiff(file1, file2);
-
-  expect(result).toEqual(testExpect);
-});
-
-test('gendiff recursion format Stylish', () => {
-  const file1 = getFixturePath('complexFile1.json');
-  const file2 = getFixturePath('complexFile2.json');
-  const testExpect = getFile(getFixturePath('expectComplexFiles.json'));
-  const result = gendiff(file1, file2);
-
-  expect(result).toEqual(testExpect);
-});
-
-test('gendiff recursion YAML format Stylish', () => {
-  const testExpect = getFile(getFixturePath('expectRecursionTree.yml'));
-  const result = gendiff(recursionTree1Yml, recursionTree2Yml);
-
-  expect(result).toEqual(testExpect);
-});
-
-test('gendiff recursion JSON format Plain', () => {
-  const testExpect = getFile(getFixturePath('expectPlain.txt'));
-  const result = gendiff(recursionTree1Yml, recursionTree2Yml, 'plain');
-
-  expect(result).toEqual(testExpect);
-});
-
-test('gendiff recursion format json', () => {
-  const testExpect = getFile(getFixturePath('jsonComplexExpected.json'));
-  const result = gendiff(recursionTree1Yml, recursionTree2Yml, 'json');
-
-  expect(result).toEqual(testExpect);
+describe('gendiff', () => {
+  test.each`
+          file1         |        file2         | format       | expectResult    | message
+  ${recursionTree1Json} |${recursionTree2Json} | ${'stylish'} | ${expectStylish}| ${'JSON stylish'}
+  ${recursionTree1Json} |${recursionTree2Json} | ${'plain'}   | ${expectPlain}  | ${'JSON plain'}
+  ${recursionTree1Yml}  |${recursionTree2Yml}  | ${'stylish'} | ${expectStylish}| ${'Yml stylish'}
+  ${recursionTree1Yml}  |${recursionTree2Yml}  | ${'plain'}   | ${expectPlain}  | ${'Yml plain'}
+`('$message',
+    ({
+      file1, file2, format, expectResult,
+    }) => {
+      expect(gendiff(file1, file2, format)).toEqual(expectResult);
+    });
 });
